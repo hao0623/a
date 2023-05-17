833717cb15fd8d8c659f5bd176648d6c31821121
hao0623 <131328836+hao0623@users.noreply.github.com>
2023-05-17 21:20:51 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(2)
+})
